8f8b4e9db76eea30dca2346757809abe20f9607b
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-09-21 11:26:49 +0530

    Sample Chechout

---
+++ cmake-build-debug/CMakeFiles/CPlusPlus_Example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusPlus_Example.dir/main.cpp.obj"
+  "CPlusPlus_Example.pdb"
+  "CPlusPlus_Example.exe"
+  "CPlusPlus_Example.exe.manifest"
+  "libCPlusPlus_Example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusPlus_Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
