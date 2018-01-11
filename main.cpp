8e401fbb82c02215749ec1cfdc21558019fd9a14
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2018-01-11 13:26:34 +0530

    Extern used

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 
+extern int a=0;
 using namespace std;
 
 int main()
 {
+
     cout<<"Enum types use";
     cout<< "defe the enumation enum enum-name { list of names } var-list; "<<endl;
     enum colours{red,green,blue,pine=5,apple} c;
@@ -13,6 +15,7 @@ int main()
     cout<< c<<endl;
     c=apple;
     cout<< c<<endl;
+
 
     return 0;
 }
