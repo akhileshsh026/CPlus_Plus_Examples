641194be8b7b17c12a51d76059d081b56fd32e9b
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2018-01-11 08:30:31 +0530

    starting from code blocks rather than clion

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
-
-// Note − A programming language is said to use static typing when type checking is performed during compile-time as opposed to run-time.
