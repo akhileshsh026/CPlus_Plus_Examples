54915f22d488fa50aa1dbd751f1943f66111350e
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2018-01-12 09:06:05 +0530

    Second way of formatting the 2D array by the  use of setW() but there are some bugs after the 1st itertion

--- main.cpp
+++ main.cpp
@@ -23,14 +23,25 @@ int main () {
 
 
 
+//   for ( int j = 0; j < 3; j++ )
+//    {
+//      for(int i=0;i<3;i++)
+//        {
+//           cout<<n[j][i]<<"\t";
+//        }
+//           cout<<endl;
+//    }
+
    for ( int j = 0; j < 3; j++ )
     {
       for(int i=0;i<3;i++)
         {
-           cout<<n[j][i]<<"\t";
+           cout<<n[j][i]<<setw(13);
         }
            cout<<endl;
     }
+
+
 
    return 0;
 
